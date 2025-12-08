2db3613fb0001a9ca155e950f835e1d292542751
Nicrayter1 <120420445+Nicrayter1@users.noreply.github.com>
2025-12-08 21:23:30 +0600

    Update productsService.js

---
+++ src/services/productsService.js
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('https://lmysveosqckpbyuldiym.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxteXN2ZW9zcWNrcGJ5dWxkaXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTMxNTksImV4cCI6MjA4MDM4OTE1OX0.z1i_Fi7uCXnX3cml7RbTHR6RxIrxVY947iOCTi80fQY');
+
+export async function getProducts() {
+  const { data } = await supabase.from('products').select('*');
+  return data;
+}
+
+export async function updateProductColumn(productId, column, value) {
+  await supabase
+    .from('products')
+    .update({ [column]: value })
+    .eq('id', productId);
+}
